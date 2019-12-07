dd8e5c565ec649923ef88a638e4a08beeb2d42f7
Valentin H <valentino00762@gmail.com>
2019-12-07 08:57:26 +0100

    initial commit

---
+++ src/test/java/com/wheels/wheelsapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheels.wheelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
